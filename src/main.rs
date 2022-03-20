f97c2d0e4d8e944b3b3f1f20d260a6ed717822d7
jpolvto <jpolvto@gmail.com>
2022-03-20 21:24:22 +0100

    renamed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 mod models;
+
+use std::fs::File;
+use std::io::Read;
 use actix_web::{web, App, HttpServer, Responder, HttpRequest};
-use serde::{Serialize, Deserialize};
 use crate::models::User;
 
-pub async fn get_users() -> impl Responder {
-    let data :Vec<User> = get_users_from_json();
-    let serialized = serde_json::to_string(&data).unwrap();
+pub struct AppStateWithData {
+    pub data :Vec<User>,
+}
+
+pub async fn get_users(data: web::Data<AppStateWithData>) -> impl Responder {
+    let serialized = serde_json::to_string(&data.data).unwrap();
     format!("{}", serialized)
 }
 
-pub async fn get_user_by_id(req: HttpRequest) -> impl Responder {
-    let data :Vec<User> = get_users_from_json();
+pub async fn get_user_by_id(req: HttpRequest, data: web::Data<AppStateWithData>) -> impl Responder {
     let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
-    let user = data.into_iter().find(|a| a.id == id).unwrap();
+    let parsed_data = &data.data;
+    let user = parsed_data.into_iter().find(|a| a.id == id).unwrap();
     let serialized = serde_json::to_string(&user).unwrap();
     format!("{}", serialized)
 }
@@ -27,9 +32,21 @@ pub async fn delete_user() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    // Load data from json file
+    let mut file = File::open("src/data.json").unwrap();
+    let mut buff = String::new();
+    file.read_to_string(&mut buff).unwrap();
+    let json_data:Vec<User> = serde_json::from_str(&mut buff).unwrap();
+
+    let data = web::Data::new(AppStateWithData {
+        data: json_data,
+    });
+
     // Start http server
     HttpServer::new(move || {
     App::new()
+        .app_data(data.clone()) // <- register the created data
         .route("/users", web::get().to(get_users))
         .route("/users/{id}", web::get().to(get_user_by_id))
         .route("/users", web::get().to(add_user))
@@ -38,51 +55,4 @@ async fn main() -> std::io::Result<()> {
         .bind("127.0.0.1:8080")?
         .run()
         .await
-}
-
-fn get_users_from_json() -> Vec<User> {
-    let users_list = r#"[
-        {
-          "id":1,
-          "email":"1@gmail.com"
-        },
-        {
-          "id":2,
-          "email":"2@gmail.com"
-        },
-        {
-          "id":3,
-          "email":"3@gmail.com"
-        },
-        {
-          "id":4,
-          "email":"4@gmail.com"
-        },
-        {
-          "id":5,
-          "email":"5@gmail.com"
-        },
-        {
-          "id":6,
-          "email":"6@gmail.com"
-        },
-        {
-          "id":7,
-          "email":"7@gmail.com"
-        },
-        {
-          "id":8,
-          "email":"8@gmail.com"
-        },
-        {
-          "id":9,
-          "email":"9@gmail.com"
-        },
-        {
-          "id":10,
-          "email":"10@gmail.com"
-        }
-        ]"#;
-
-    return serde_json::from_str(users_list).unwrap();
 }
