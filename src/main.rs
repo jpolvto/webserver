e0b7ce118b9d7c686dead7a49e0bf8336390f816
jpolvto <jpolvto@gmail.com>
2022-03-20 20:09:36 +0100

    renamed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod models;
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+use serde::{Serialize, Deserialize};
+use crate::models::User;
 
 pub async fn get_users() -> impl Responder {
-    format!("hello from get users by id")
+    let data :Vec<User> = get_users_from_json();
+    let serialized = serde_json::to_string(&data).unwrap();
+    format!("{}", serialized)
 }
 
-pub async fn get_user_by_id() -> impl Responder {
-    format!("hello from get users by id")
+pub async fn get_user_by_id(req: HttpRequest) -> impl Responder {
+    let data :Vec<User> = get_users_from_json();
+    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
+    let user = data.into_iter().find(|a| a.id == id).unwrap();
+    let serialized = serde_json::to_string(&user).unwrap();
+    format!("{}", serialized)
 }
 
 pub async fn add_user() -> impl Responder {
@@ -16,7 +24,6 @@ pub async fn add_user() -> impl Responder {
 pub async fn delete_user() -> impl Responder {
     format!("hello from delete user")
 }
-
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -31,4 +38,51 @@ async fn main() -> std::io::Result<()> {
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+fn get_users_from_json() -> Vec<User> {
+    let users_list = r#"[
+        {
+          "id":1,
+          "email":"1@gmail.com"
+        },
+        {
+          "id":2,
+          "email":"2@gmail.com"
+        },
+        {
+          "id":3,
+          "email":"3@gmail.com"
+        },
+        {
+          "id":4,
+          "email":"4@gmail.com"
+        },
+        {
+          "id":5,
+          "email":"5@gmail.com"
+        },
+        {
+          "id":6,
+          "email":"6@gmail.com"
+        },
+        {
+          "id":7,
+          "email":"7@gmail.com"
+        },
+        {
+          "id":8,
+          "email":"8@gmail.com"
+        },
+        {
+          "id":9,
+          "email":"9@gmail.com"
+        },
+        {
+          "id":10,
+          "email":"10@gmail.com"
+        }
+        ]"#;
+
+    return serde_json::from_str(users_list).unwrap();
 }
