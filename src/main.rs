b4cbd2cb061e07b7007d1e5fab65050c93a2de35
jpolvto <jpolvto@gmail.com>
2022-03-23 16:57:12 +0100

    added delete and post

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-use bson::{doc, Document};
+use bson::{ doc, Document};
 use futures_util::stream::StreamExt;
 use mongodb::{Client, Collection};
 use mongodb::options::ClientOptions;
+use serde::{ Serialize, Deserialize};
 
 pub struct AppState {
     pub data: Collection<Document>,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    id: i32,
+    email: String
+}
+
 
 pub async fn all_users(data: web::Data<AppState>) -> impl Responder {
 
@@ -39,6 +47,24 @@ pub async fn get_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl
     HttpResponse::Ok().json(results)
 }
 
+pub async fn delete_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
+    let result = data.data.find_one_and_delete(doc! { "id":  id }, None).await.unwrap();
+    let mut results: Vec<Document> = Vec::new();
+
+    if let Some(doc) = result {
+        results.push(doc);
+    }
+
+    HttpResponse::Ok().json(results)
+}
+
+pub async fn post_user(info: web::Json<User>, data: web::Data<AppState>) -> impl Responder {
+
+    let result = data.data.insert_one(doc! { "id": &info.id, "email": &info.email }, None).await.unwrap();
+    HttpResponse::Ok().json(result.inserted_id)
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let options = ClientOptions::parse("mongodb://127.0.0.1:27017").await.unwrap();
@@ -49,7 +75,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(db.collection::<Document>("users"))
             .route("/users", web::get().to(all_users))
+            .route("/users", web::post().to(post_user))
             .route("/users/{id}", web::get().to(get_user_by_id))
+            .route("/users/{id}", web::delete().to(delete_user_by_id))
     })
         .bind("127.0.0.1:8080")?
         .run()
