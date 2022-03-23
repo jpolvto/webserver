9a743b9b7c735869e72518faed306bfec7d9b41b
jpolvto <jpolvto@gmail.com>
2022-03-23 17:12:17 +0100

    spread in methods

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,12 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-use bson::{ doc, Document};
-use futures_util::stream::StreamExt;
-use mongodb::{Client, Collection};
+mod router;
+mod models;
+
+use actix_web::{App, HttpServer, web};
+use bson::{ Document};
+use mongodb::{Client};
 use mongodb::options::ClientOptions;
-use serde::{ Serialize, Deserialize};
 
-pub struct AppState {
-    pub data: Collection<Document>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    id: i32,
-    email: String
-}
-
-
-pub async fn all_users(data: web::Data<AppState>) -> impl Responder {
-
-    let mut cursor = data.data.find(doc! {}, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
-
-    while let Some(result) = cursor.next().await {
-        match result {
-            Ok(document) => {
-                results.push(document);
-            }
-            Err(_) => {
-                return HttpResponse::InternalServerError().finish();
-            }
-        }
-    }
-
-    HttpResponse::Ok().json(results)
-}
-
-pub async fn get_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
-    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
-    let result = data.data.find_one(doc! { "id":  id }, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
-
-    if let Some(doc) = result {
-        results.push(doc);
-    }
-
-    HttpResponse::Ok().json(results)
-}
-
-pub async fn delete_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
-    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
-    let result = data.data.find_one_and_delete(doc! { "id":  id }, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
-
-    if let Some(doc) = result {
-        results.push(doc);
-    }
-
-    HttpResponse::Ok().json(results)
-}
-
-pub async fn post_user(info: web::Json<User>, data: web::Data<AppState>) -> impl Responder {
-
-    let result = data.data.insert_one(doc! { "id": &info.id, "email": &info.email }, None).await.unwrap();
-    HttpResponse::Ok().json(result.inserted_id)
-}
+use crate::router::{all_users, delete_user_by_id, get_user_by_id, post_user};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
