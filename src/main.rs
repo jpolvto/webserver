1a6ec1b1878f997c42016034bb243050d83bb755
jpolvto <jpolvto@gmail.com>
2022-03-24 23:14:14 +0100

    mongodb atlas connection

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,87 @@
-mod routes;
-mod models;
-
-use actix_web::{App, HttpServer, web};
-use bson::{ Document};
-use mongodb::{Client};
+use std::env;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use bson::{doc, Document};
+use futures_util::stream::StreamExt;
+use mongodb::{Client, Collection};
 use mongodb::options::ClientOptions;
+use dotenv;
+use serde::{ Serialize, Deserialize};
 
-use crate::routes::{all_users, delete_user_by_id, get_user_by_id, post_user};
+pub struct AppState {
+    pub data: Collection<Document>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: i32,
+    pub email: String
+}
+
+pub async fn all_users(data: web::Data<AppState>) -> impl Responder {
+
+    let mut cursor = data.data.find(doc! {}, None).await.unwrap();
+    let mut results: Vec<Document> = Vec::new();
+
+    while let Some(result) = cursor.next().await {
+        match result {
+            Ok(document) => {
+                results.push(document);
+            }
+            Err(_) => {
+                return HttpResponse::InternalServerError().finish();
+            }
+        }
+    }
+
+    HttpResponse::Ok().json(results)
+}
+
+pub async fn get_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
+    let result = data.data.find_one(doc! { "id":  id }, None).await.unwrap();
+    let mut results: Vec<Document> = Vec::new();
+
+    if let Some(doc) = result {
+        results.push(doc);
+    }
+
+    HttpResponse::Ok().json(results)
+}
+
+pub async fn post_user(info: web::Json<User>, data: web::Data<AppState>) -> impl Responder {
+
+    let result = data.data.insert_one(doc! { "id": &info.id, "email": &info.email }, None).await.unwrap();
+    HttpResponse::Ok().json(doc! { "_id": result.inserted_id, "id": &info.id, "email": &info.email })
+}
+
+pub async fn delete_user_by_id(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
+    let result = data.data.find_one_and_delete(doc! { "id":  id }, None).await.unwrap();
+    let mut results: Vec<Document> = Vec::new();
+
+    if let Some(doc) = result {
+        results.push(doc);
+    }
+
+    HttpResponse::Ok().json(results)
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let options = ClientOptions::parse("mongodb://127.0.0.1:27017").await.unwrap();
+    dotenv::dotenv().ok();
+    let host_name = format!("mongodb+srv://{}:{}@cluster0.17s4f.mongodb.net/actix-webserver?retryWrites=true&w=majority", env::var("user").unwrap(), env::var("password").unwrap());
+    let options = ClientOptions::parse(&host_name).await.unwrap();
     let client = Client::with_options(options).unwrap();
-    let db = client.database("actix-web");
+    let db = client.database("actix-webserver");
+    let user_collection: Collection<Document> = db.collection("users");
+
+    let data = web::Data::new(AppState {
+        data: user_collection,
+    });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(db.collection::<Document>("users"))
+            .app_data(data.clone()) // <- register the created data
             .route("/users", web::get().to(all_users))
             .route("/users", web::post().to(post_user))
             .route("/users/{id}", web::get().to(get_user_by_id))
