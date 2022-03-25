2355f705ca11b5c66863a171b35523afe6e8e2e2
jpolvto <jpolvto@gmail.com>
2022-03-25 13:35:35 +0100

    added user_from_document function

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
-use bson::Document;
+use bson::{Document};
+use std::string::String;
 use mongodb::Collection;
 use serde::{Serialize, Deserialize};
 
@@ -10,4 +11,22 @@ pub struct AppState {
 pub struct User {
     pub id: i32,
     pub email: String
+}
+
+pub fn user_from_document(document: bson::Document) -> Option<User> {
+
+    let _id = document.get("id");
+    let _email = document.get("email");
+
+    return match (_id, _email) {
+        (Some(_id), Some(_email)) => {
+            let a = _id.as_i32();
+            let b = _email.as_str();
+            return match (a, b) {
+                (Some(a), Some(b)) => return Some(User{ id: a, email: b.to_string() }),
+                _ => None,
+            }
+        },
+        _ => None,
+    }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,17 +1,21 @@
 use actix_web::{HttpRequest, HttpResponse, Responder, web};
-use bson::{doc, Document};
+use bson::{doc};
 use futures_util::StreamExt;
 use crate::models;
+use crate::models::{User, user_from_document};
 
 pub async fn all_users(data: web::Data<models::AppState>) -> impl Responder {
 
     let mut cursor = data.data.find(doc! {}, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
+    let mut results: Vec<User> = Vec::new();
 
     while let Some(result) = cursor.next().await {
         match result {
             Ok(document) => {
-                results.push(document);
+                let optional_user = user_from_document(document);
+                if let Some(user) = optional_user {
+                    results.push(user);
+                }
             }
             Err(_) => {
                 return HttpResponse::InternalServerError().finish();
@@ -25,17 +29,19 @@ pub async fn all_users(data: web::Data<models::AppState>) -> impl Responder {
 pub async fn get_user_by_id(req: HttpRequest, data: web::Data<models::AppState>) -> impl Responder {
     let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
     let result = data.data.find_one(doc! { "id":  id }, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
+    let mut results: Vec<User> = Vec::new();
 
     if let Some(doc) = result {
-        results.push(doc);
+        let optional_user = user_from_document(doc);
+        if let Some(user) = optional_user {
+            results.push(user);
+        }
     }
 
     HttpResponse::Ok().json(results)
 }
 
 pub async fn post_user(info: web::Json<models::User>, data: web::Data<models::AppState>) -> impl Responder {
-
     let result = data.data.insert_one(doc! { "id": &info.id, "email": &info.email }, None).await.unwrap();
     HttpResponse::Ok().json(doc! { "_id": result.inserted_id, "id": &info.id, "email": &info.email })
 }
@@ -43,10 +49,15 @@ pub async fn post_user(info: web::Json<models::User>, data: web::Data<models::Ap
 pub async fn delete_user_by_id(req: HttpRequest, data: web::Data<models::AppState>) -> impl Responder {
     let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
     let result = data.data.find_one_and_delete(doc! { "id":  id }, None).await.unwrap();
-    let mut results: Vec<Document> = Vec::new();
+
+
+    let mut results: Vec<User> = Vec::new();
 
     if let Some(doc) = result {
-        results.push(doc);
+        let optional_user = user_from_document(doc);
+        if let Some(user) = optional_user {
+            results.push(user);
+        }
     }
 
     HttpResponse::Ok().json(results)
