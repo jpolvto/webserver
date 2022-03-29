6b0059b90e79fbce34ae9b25508896b6f7ac84b9
jpolvto <jpolvto@gmail.com>
2022-03-29 13:13:49 +0200

    added function to avoid duplicate code

--- src/routes.rs
+++ src/routes.rs
@@ -1,19 +1,13 @@
 use bson::{doc, Document};
 use futures_util::StreamExt;
-use mongodb::Collection;
+use mongodb::{Collection, Cursor};
 use crate::models;
 use crate::models::User;
-use actix_web::{get, post, delete, web::Json, web, HttpRequest};
+use actix_web::{post, get, delete, web::Json, web, HttpRequest};
 use mongodb::results::{DeleteResult, InsertManyResult};
 use crate::errors::AppError;
 
-
-#[get("/users")]
-pub async fn all_users(data: web::Data<models::AppState>) -> Result<Json<Vec<User>>, AppError> {
-
-    let user_collection: Collection<Document> = data.db.collection("users");
-    let mut cursor= user_collection.find(doc! { }, None).await?;
-
+pub async fn get_users_from_cursor (mut cursor: Cursor<Document>) -> Result<Vec<User>, AppError> {
     let mut results: Vec<User> = Vec::new();
 
     while let Some(result) = cursor.next().await {
@@ -31,8 +25,21 @@ pub async fn all_users(data: web::Data<models::AppState>) -> Result<Json<Vec<Use
     }
 
     if results.is_empty() {
-        return Err(AppError::NotFound)
+        return Err(AppError::NotFound);
     }
+
+    Ok(results)
+
+}
+
+
+
+#[get("/users")]
+pub async fn all_users(data: web::Data<models::AppState>) -> Result<Json<Vec<User>>, AppError> {
+
+    let user_collection: Collection<Document> = data.db.collection("users");
+    let cursor= user_collection.find(doc! { }, None).await?;
+    let results = get_users_from_cursor(cursor).await?;
 
     Ok(Json(results))
 
@@ -52,46 +59,11 @@ pub async fn get_users_by_id(req: HttpRequest, data: web::Data<models::AppState>
         }
     };
 
-
     let user_collection: Collection<Document> = data.db.collection("users");
-    let mut cursor= user_collection.find(doc! { "id": id }, None).await?;
-    let mut results: Vec<User> = Vec::new();
-
-    while let Some(result) = cursor.next().await {
-        match result {
-            Ok(document) => {
-                let optional_user = User::from_document(document);
-                if let Some(user) = optional_user {
-                    results.push(user);
-                }
-            }
-            Err(err) => {
-                drop(err);
-            }
-        }
-    }
-
-    if results.is_empty() {
-        return Err(AppError::NotFound)
-    }
+    let cursor= user_collection.find(doc! { "id": id }, None).await?;
+    let results = get_users_from_cursor(cursor).await?;
 
     Ok(Json(results))
-}
-
-#[post("/users")]
-pub async fn post_users(info: web::Json<Vec<User>>, data: web::Data<models::AppState>) -> Result<Json<InsertManyResult>, AppError> {
-
-    let mut docs: Vec<Document> = Vec::new();
-
-    for user in info.0 {
-        let doc = User::into(user);
-        docs.push(doc)
-    }
-
-    let user_collection: Collection<Document> = data.db.collection("users");
-    let result =  user_collection.insert_many(docs, None).await?;
-
-    Ok(Json(result))
 }
 
 #[delete("/users/{id}")]
@@ -114,3 +86,22 @@ pub async fn delete_user_by_id(req: HttpRequest, data: web::Data<models::AppStat
     Ok(Json(result))
 
 }
+
+
+#[post("/users")]
+pub async fn post_users(info: web::Json<Vec<User>>, data: web::Data<models::AppState>) -> Result<Json<InsertManyResult>, AppError> {
+
+    let mut docs: Vec<Document> = Vec::new();
+
+    for user in info.0 {
+        let doc = User::into(user);
+        docs.push(doc)
+    }
+
+    let user_collection: Collection<Document> = data.db.collection("users");
+    let result =  user_collection.insert_many(docs, None).await?;
+
+    Ok(Json(result))
+}
+
+
